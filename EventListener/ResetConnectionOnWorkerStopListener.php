15fe11392c5033f61c810aad3d1168d7ee7d2b2c
Sander Hagen <28562438+SanderHagen@users.noreply.github.com>
2022-03-25 09:34:49 +0100

    add logging

--- EventListener/ResetConnectionOnWorkerStopListener.php
+++ EventListener/ResetConnectionOnWorkerStopListener.php
@@ -2,16 +2,29 @@
 
 namespace Symfony\Component\Messenger\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\Event\WorkerStoppedEvent;
 use Symfony\Contracts\Service\ResetInterface;
 
 class ResetConnectionOnWorkerStopListener implements EventSubscriberInterface
 {
+    private $logger = null;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onWorkerStopped(WorkerStoppedEvent $event): void
     {
+        if ($this->logger) {
+            $this->logger->debug('Reset connection called');
+        }
         foreach($event->getReceivers() as $transportName => $receiver) {
             if ($receiver instanceof ResetInterface) {
+                if ($this->logger) {
+                    $this->logger->debug(sprintf('Receiver for %s reset', $transportName));
+                }
                 $receiver->reset();
             }
         }
